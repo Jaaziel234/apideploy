daf0f6558ea0f2f1305c800bf255b6e2e177dc7a
Jaaziel234 <jaazielmc45@gmail.com>
2024-11-20 13:56:51 -0600

    Primer commit de api a produccion con carpeta7

--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@ require("dotenv").config();
 
 const express = require("express");
 
-const port = process.env.PORT || 6000; // Asegúrate de usar un puerto por defecto en caso de que no esté definido
+const port = process.env.PORT || 5000; // Asegúrate de usar un puerto por defecto en caso de que no esté definido
+
+const cors = require('cors');
 
 const app = express();
+
+// Configurar CORS para permitir solicitudes de tu dominio local
+app.use(cors({
+    origin: '*', // Cambia esto por tu dominio o usa '*' para permitir cualquier origen
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Asegúrate de permitir los métodos que usas
+    allowedHeaders: ['Content-Type'] // Si necesitas otros encabezados personalizados, agrégales
+}));
 
 app.get("/", (req, res) => {
     res.send({
