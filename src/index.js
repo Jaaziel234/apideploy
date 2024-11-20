2736547b930235c3b82275d3a1f9e3e0b33bb688
Jaaziel234 <jaazielmc45@gmail.com>
2024-11-20 12:27:57 -0600

    Primer commit de api a produccion con carpeta6

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 
-const port = process.env.PORT || 5000; // Asegúrate de usar un puerto por defecto en caso de que no esté definido
+const port = process.env.PORT || 6000; // Asegúrate de usar un puerto por defecto en caso de que no esté definido
 
 const app = express();
 
